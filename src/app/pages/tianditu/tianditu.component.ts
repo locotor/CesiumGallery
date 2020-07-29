19305cdbfb8f9601cf54f67b54a02c88540161e5
locotor <locotor@outlook.com>
2020-07-29 15:23:00 +0800

    add tianditu component to gallery

--- src/app/pages/tianditu/tianditu.component.ts
+++ src/app/pages/tianditu/tianditu.component.ts
@@ -1,4 +1,7 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { CesiumViewerOption } from '@shared/models/models';
+import { BaseMapComponent } from '@shared/components/base-map/base-map.component';
+import Layers from '@shared/utilities/cesium/Layers';
 import * as Cesium from 'cesium';
 
 @Component({
@@ -7,7 +10,15 @@ import * as Cesium from 'cesium';
 })
 export class TiandituComponent implements AfterViewInit {
 
-  viewerOpts = {
+  @ViewChild('baseMap') baseMap: BaseMapComponent;
+
+  private viewer: any;
+  private scene: any;
+  private camera: any;
+  private viewModel: any;
+  private layer: Layers;
+  private imageryType: string;
+  viewerOpts: CesiumViewerOption = {
     animation: false,
     fullscreenButton: false,
     geocoder: false,
@@ -17,14 +28,99 @@ export class TiandituComponent implements AfterViewInit {
     selectionIndicator: false,
     timeline: false,
     navigationHelpButton: false,
-    imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
-      url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
-    }),
+    imageryProvider: new Cesium.WebMapTileServiceImageryProvider({
+      url: 'http://t0.tianditu.com/vec_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=vec&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles',
+        layer: 'tdtVecBasicLayer',
+      style: 'default',
+      format: 'image/jpeg',
+      tileMatrixSetID: 'GoogleMapsCompatible',
+    })
   };
 
   constructor() { }
 
-  ngAfterViewInit(): void {
+  ngAfterViewInit() {
+    this.initCesium();
+    this.initLocation();
+    this.initImagery();
+    this.initLayerPicker();
   }
 
+  initCesium() {
+    this.viewer = this.baseMap.viewer;
+    this.scene = this.viewer.scene;
+    this.camera = this.scene.camera;
+    this.viewModel = this.viewer.baseLayerPicker.viewModel;
+  }
+
+  initLocation() {
+    this.camera.flyTo({
+      destination: Cesium.Cartesian3.fromDegrees(108.43130, 37.76237, 7579376)
+    });
+  }
+
+  initImagery() {
+    this.layer = new Layers(this.viewer);
+  }
+
+  initLayerPicker() {
+    // 修改表面样式
+    const boxList = this.viewer.baseLayerPicker.container.lastChild.children;
+    boxList[0].innerText = '地图';
+    boxList[2].innerText = '地形';
+    // 地形内容重设
+    this.resetTerrain();
+    // 底图内容重设
+    this.imageryType = location.hash.replace(/(.*)\?type=(.*)$/, '$2');
+    const viewModels = this.initTianDiLayer();
+    if (this.imageryType === 'terrain') {
+      this.viewModel.imageryProviderViewModels = [];
+      this.viewModel.terrainProviderViewModels = viewModels;
+    } else {
+      this.viewModel.imageryProviderViewModels = viewModels;
+      this.viewModel.selectedImagery = viewModels[0];
+    }
+  }
+
+  resetTerrain() {
+    // 修改地形展示内容
+    const terrainViewModels = [];
+    terrainViewModels.push(new Cesium.ProviderViewModel({
+      name: '无地形',
+      iconUrl: Cesium.buildModuleUrl('../../assets/cesium/Widgets/Images/TerrainProviders/Ellipsoid.png'),
+      tooltip: 'WGS84标准球体',
+      creationFunction: () => new Cesium.EllipsoidTerrainProvider({})
+    }));
+    terrainViewModels.push(new Cesium.ProviderViewModel({
+      name: '全球地形',
+      iconUrl: Cesium.buildModuleUrl('../../assets/cesium/Widgets/Images/TerrainProviders/CesiumWorldTerrain.png'),
+      tooltip: 'Cesium官方高分辨率全球地形',
+      creationFunction: () => Cesium.createWorldTerrain({
+        requestWaterMask: true,
+        requestVertexNormals: true
+      })
+    }));
+    this.viewModel.terrainProviderViewModels = terrainViewModels;
+    this.viewModel.selectedTerrain = terrainViewModels[0];
+  }
+
+  initTianDiLayer() {
+    document.title = '天地图服务';
+    const imageryViewModels = [];
+    this.layer.setTianDiToken('1dbacbfc4d4746922bebc0591e443351');
+    imageryViewModels.push(new Cesium.ProviderViewModel({
+      name: '天地图矢量',
+      iconUrl: Cesium.buildModuleUrl('../../assets/images/imagery/tiandi_vector.png'),
+      tooltip: '天地图矢量地图服务',
+      creationFunction: () => this.layer.getImageryLayer(Layers.TYPE.TianDi.VECTOR)
+    }));
+    imageryViewModels.push(new Cesium.ProviderViewModel({
+      name: '天地图影像',
+      iconUrl: Cesium.buildModuleUrl('../../assets/images/imagery/tiandi_image.png'),
+      tooltip: '天地图影像地图服务',
+      creationFunction: () => this.layer.getImageryLayer(Layers.TYPE.TianDi.IMAGE)
+    }));
+    return imageryViewModels;
+  }
 }
+
---
+++ src/app/shared/utilities/cesium/Layers.ts
@@ -0,0 +1,240 @@
+import * as Cesium from 'cesium';
+
+class ArcGIS {
+  public static readonly IMAGE = Symbol('IMAGE');
+  public static readonly GREY = Symbol('GREY');
+  public static readonly BLUE = Symbol('BLUE');
+  public static readonly COMMUNITY = Symbol('COMMUNITY');
+}
+
+class TianDi {
+  public static readonly IMAGE = Symbol('IMAGE');
+  public static readonly VECTOR = Symbol('VECTOR');
+  public static readonly TERRAIN = Symbol('TERRAIN');
+  public static readonly GLOBAL = Symbol('GLOBAL');
+}
+
+class Baidu {
+  public static readonly VECTOR = Symbol('VECTOR');
+  public static readonly IMAGE = Symbol('IMAGE');
+  public static readonly GRAY = Symbol('GRAY');
+  public static readonly MIDNIGHT = Symbol('MIDNIGHT');
+  public static readonly DARK = Symbol('DARK');
+  public static readonly ALARM = Symbol('ALARM');
+  public static readonly GREEN = Symbol('GREEN');
+  public static readonly BLUE = Symbol('BLUE');
+}
+
+class MapBox {
+  public static readonly STREETS = Symbol('STREETS');
+  public static readonly STREETS_BASIC = Symbol('STREETS_BASIC');
+  public static readonly SATELLITE = Symbol('SATELLITE');
+  public static readonly STREETS_SATELLITE = Symbol('STREETS_SATELLITE');
+  public static readonly LIGHT = Symbol('LIGHT');
+  public static readonly DARK = Symbol('DARK');
+  public static readonly WHEATPASTE = Symbol('WHEATPASTE');
+  public static readonly COMIC = Symbol('COMIC');
+  public static readonly OUTDOORS = Symbol('OUTDOORS');
+  public static readonly RUN_BIKE_HIKE = Symbol('RUN_BIKE_HIKE');
+  public static readonly PENCIL = Symbol('PENCIL');
+  public static readonly PIRATES = Symbol('PIRATES');
+  public static readonly EMERALD = Symbol('EMERALD');
+  public static readonly HIGH_CONTRAST = Symbol('HIGH_CONTRAST');
+}
+
+class Terrain {
+  public static readonly NO_TERRAIN = Symbol('NO_TERRAIN');
+  public static readonly GLOBAL_TERRAIN = Symbol('GLOBAL_TERRAIN');
+  public static readonly CHINA_TERRAIN = Symbol('CHINA_TERRAIN');
+}
+
+export default class Layers {
+
+  public static readonly TYPE = {
+    ArcGIS,
+    TianDi,
+    Baidu,
+    MapBox,
+    Terrain
+  };
+
+  private mapBoxToken = '';
+  private tianDiToken = '';
+
+  private readonly imageries = {
+    // ArcGIS
+    [ArcGIS.IMAGE]: [new Cesium.ArcGisMapServerImageryProvider({
+      url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
+    })],
+    [ArcGIS.COMMUNITY]: [new Cesium.ArcGisMapServerImageryProvider({
+      url: 'http://cache1.arcgisonline.cn/arcgis/rest/services/ChinaOnlineCommunity/MapServer'
+    })],
+    [ArcGIS.GREY]: [new Cesium.ArcGisMapServerImageryProvider({
+      url: 'http://cache1.arcgisonline.cn/arcgis/rest/services/ChinaOnlineStreetGray/MapServer',
+    })],
+    [ArcGIS.BLUE]: [new Cesium.ArcGisMapServerImageryProvider({
+      url: 'https://map.geoq.cn/arcgis/rest/services/ChinaOnlineStreetPurplishBlue/MapServer',
+    })],
+
+    // 天地图
+    [TianDi.VECTOR]: null,
+    [TianDi.IMAGE]: null,
+    [TianDi.TERRAIN]: null,
+    [TianDi.GLOBAL]: null,
+
+    // MapBox 需要token才能加载
+    [MapBox.STREETS]: null,
+    [MapBox.STREETS_BASIC]: null,
+    [MapBox.SATELLITE]: null,
+    [MapBox.STREETS_SATELLITE]: null,
+    [MapBox.LIGHT]: null,
+    [MapBox.DARK]: null,
+    [MapBox.WHEATPASTE]: null,
+    [MapBox.COMIC]: null,
+    [MapBox.OUTDOORS]: null,
+    [MapBox.RUN_BIKE_HIKE]: null,
+    [MapBox.PENCIL]: null,
+    [MapBox.PIRATES]: null,
+    [MapBox.EMERALD]: null,
+    [MapBox.HIGH_CONTRAST]: null,
+  };
+  private readonly terrains = {
+    // Terrain
+    [Terrain.NO_TERRAIN]: new Cesium.EllipsoidTerrainProvider({
+      ellipsoid: Cesium.Ellipsoid.WGS84
+    }),
+    [Terrain.GLOBAL_TERRAIN]: new Cesium.CesiumTerrainProvider({
+      url: Cesium.IonResource.fromAssetId(1),
+      requestWaterMask: !0,
+      requestVertexNormals: !0
+    }),
+    [Terrain.CHINA_TERRAIN]: new Cesium.CesiumTerrainProvider({
+      url: 'http://localhost:8080/terrain', // 'http://192.168.11.181:8080/terrain',
+      requestWaterMask: !0,
+      requestVertexNormals: !0
+    })
+  };
+
+  private viewer: any;
+
+  private imageryType = null;
+  private terrainType = null;
+
+  constructor(viewer) {
+    this.viewer = viewer;
+  }
+
+  setImageryLayer(type: symbol) {
+    this.viewer.imageryLayers.removeAll();
+    this.imageryType = type;
+    const imageryLayers = this.imageries[type];
+    this.checkToken(type);
+    imageryLayers.forEach(layer => {
+      this.viewer.imageryLayers.addImageryProvider(layer);
+    });
+    return imageryLayers;
+  }
+
+  getImageryLayer(type: symbol) {
+    return this.imageries[type];
+  }
+
+  setTerrainLayer(type: symbol) {
+    this.terrainType = type;
+    const terrainLayer = this.terrains[type];
+    this.viewer.terrainProvider = terrainLayer;
+    return terrainLayer;
+  }
+
+  getTerrainLayer(type: symbol) {
+    return this.terrains[type];
+  }
+
+  // TODO: 针对一些需要添加key值或者token的底图服务
+  checkToken(type) {
+  }
+
+  // 设置天地图的token
+  setTianDiToken(token) {
+    this.tianDiToken = token;
+    this.imageries[TianDi.VECTOR] = [new Cesium.WebMapTileServiceImageryProvider({
+      // tslint:disable: max-line-length
+      url: 'http://t0.tianditu.com/vec_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=vec&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles&tk=' + this.tianDiToken,
+      layer: 'tdtVecBasicLayer',
+      style: 'default',
+      format: 'image/jpeg',
+      tileMatrixSetID: 'GoogleMapsCompatible',
+    })];
+    this.imageries[TianDi.IMAGE] = [new Cesium.WebMapTileServiceImageryProvider({
+      url: 'http://t0.tianditu.com/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles&tk=' + this.tianDiToken,
+      layer: 'tdtVImgBasicLayer',
+      style: 'default',
+      format: 'image/jpeg',
+      tileMatrixSetID: 'GoogleMapsCompatible',
+    })];
+  }
+
+  // 设置mapBox的token
+  setMapBoxToken(token) {
+    // tslint:disable-next-line: max-line-length
+    // mapbox可选值 mapbox.satellite, mapbox.streets, mapbox.light, mapbox.dark, mapbox.streets-satellite, mapbox.wheatpaste, mapbox.streets-basic, mapbox.comic, mapbox.outdoors, mapbox.run-bike-hike, mapbox.pencil, mapbox.pirates, mapbox.emerald, mapbox.high-contrast
+    this.mapBoxToken = token;
+    this.imageries[MapBox.STREETS] = [new Cesium.MapboxImageryProvider({
+      mapId: 'mapbox.streets',
+      accessToken: this.mapBoxToken
+    })];
+    this.imageries[MapBox.STREETS_BASIC] = [new Cesium.MapboxImageryProvider({
+      mapId: 'mapbox.streets-basic',
+      accessToken: this.mapBoxToken
+    })];
+    this.imageries[MapBox.SATELLITE] = [new Cesium.MapboxImageryProvider({
+      mapId: 'mapbox.satellite',
+      accessToken: this.mapBoxToken
+    })];
+    this.imageries[MapBox.STREETS_SATELLITE] = [new Cesium.MapboxImageryProvider({
+      mapId: 'mapbox.streets-satellite',
+      accessToken: this.mapBoxToken
+    })];
+    this.imageries[MapBox.LIGHT] = [new Cesium.MapboxImageryProvider({
+      mapId: 'mapbox.light',
+      accessToken: this.mapBoxToken
+    })];
+    this.imageries[MapBox.DARK] = [new Cesium.MapboxImageryProvider({
+      mapId: 'mapbox.dark',
+      accessToken: this.mapBoxToken
+    })];
+    this.imageries[MapBox.WHEATPASTE] = [new Cesium.MapboxImageryProvider({
+      mapId: 'mapbox.wheatpaste',
+      accessToken: this.mapBoxToken
+    })];
+    this.imageries[MapBox.COMIC] = [new Cesium.MapboxImageryProvider({
+      mapId: 'mapbox.comic',
+      accessToken: this.mapBoxToken
+    })];
+    this.imageries[MapBox.OUTDOORS] = [new Cesium.MapboxImageryProvider({
+      mapId: 'mapbox.outdoors',
+      accessToken: this.mapBoxToken
+    })];
+    this.imageries[MapBox.RUN_BIKE_HIKE] = [new Cesium.MapboxImageryProvider({
+      mapId: 'mapbox.run-bike-hike',
+      accessToken: this.mapBoxToken
+    })];
+    this.imageries[MapBox.PENCIL] = [new Cesium.MapboxImageryProvider({
+      mapId: 'mapbox.pencil',
+      accessToken: this.mapBoxToken
+    })];
+    this.imageries[MapBox.PIRATES] = [new Cesium.MapboxImageryProvider({
+      mapId: 'mapbox.pirates',
+      accessToken: this.mapBoxToken
+    })];
+    this.imageries[MapBox.EMERALD] = [new Cesium.MapboxImageryProvider({
+      mapId: 'mapbox.emerald',
+      accessToken: this.mapBoxToken
+    })];
+    this.imageries[MapBox.HIGH_CONTRAST] = [new Cesium.MapboxImageryProvider({
+      mapId: 'mapbox.high-contrast',
+      accessToken: this.mapBoxToken
+    })];
+  }
+
+}
